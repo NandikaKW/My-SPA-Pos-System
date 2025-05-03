d58a125185159f11952dfddaad2653c59f50122a
LENOVO <nandikakaweesha4005@gmail.com>
2025-05-03 20:17:40 +0530

    new commit

---
+++ controller/customerController.js
@@ -0,0 +1,7 @@
+import{customer_db} from "../db/db.js";
+import {CustomerModel} from "../model/customerModel.js";
+
+const submit = $('#Customer .btn-success').eq(0);
+const update = $('#Customer .btn-primary').eq(0);
+const delete_btn = $('#Customer .btn-danger').eq(0);
+const reset = $('#Customer .btn-warning').eq(0);
