72d1a631397e9911b090b0030cc82236837d80b6
LENOVO <nandikakaweesha4005@gmail.com>
2025-05-03 20:32:40 +0530

    customerController commit

--- controller/customerController.js
+++ controller/customerController.js
@@ -25,3 +25,57 @@ const mobilePattern = /^(?:\+94|0|94|0094)?(7[0-9])(\d{7})$/;
 const namePattern = /^[\p{L}\s.'-]+$/u;
 // Address pattern to ensure minimum address quality
 const addressPattern = /^[\p{L}0-9\s.,'-]{10,}$/u;
+
+$(document).ready(() => {
+    customer_id.val(generateCustomerId());
+    populateCustomerTable();
+});
+
+$('#customer_page').on('click', () => {
+    resetForm();
+    populateCustomerTable();
+});
+
+searchField.on('input', () => searchCustomers());
+searchBtn.on('click', () => searchCustomers());
+
+submit.on('click', (e) => {
+    e.preventDefault();
+    addCustomer();
+});
+
+update.on('click', () => updateCustomer());
+
+delete_btn.on('click', () => deleteCustomer());
+
+reset.on('click', (e) => {
+    e.preventDefault();
+    resetForm();
+});
+
+
+function generateCustomerId() {
+    if (customer_db.length === 0) return 'cust-001';
+
+    const highestId = customer_db.reduce((max, customer) => {
+        const num = parseInt(customer.customer_id.split('-')[1]);
+        return num > max ? num : max;
+    }, 0);
+
+    return `cust-${String(highestId + 1).padStart(3, '0')}`;
+}
+
+function resetForm() {
+    customer_id.val(generateCustomerId());
+    name.val('');
+    address.val('');
+    contact.val('');
+    email.val('');
+    submit.prop("disabled", false);
+    delete_btn.prop("disabled", true);
+    update.prop("disabled", true);
+
+    // Clear validation states
+    $('.is-invalid').removeClass('is-invalid');
+    $('.invalid-feedback').remove();
+}
