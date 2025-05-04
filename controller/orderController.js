db0ac6b0540f6a7a575c9d49294cbad2bc541c4f
LENOVO <nandikakaweesha4005@gmail.com>
2025-05-04 08:43:51 +0530

    new commit

---
+++ controller/orderController.js
@@ -0,0 +1,11 @@
+import {item_db} from "../db/db.js";
+import {ItemModel} from "../model/itemModel.js";
+
+import {customer_db} from "../db/db.js";
+import {CustomerModel} from "../model/customerModel.js";
+
+import {order_db} from "../db/db.js";
+import {OrderModel} from "../model/orderModel.js";
+
+import {order_details_db} from "../db/db.js";
+import {orderModel} from "../model/orderDetailsModel.js";
